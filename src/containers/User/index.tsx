e3b12e4fbec023a484494db36b9020dc7f8dfffa
Ehsan <zerob4wl@gmail.com>
2017-08-27 14:15:27 +0430

    remove register components

--- src/containers/User/index.tsx
+++ src/containers/User/index.tsx
@@ -3,7 +3,6 @@ import {Route, RouteComponentProps} from "react-router";
 import {RootState} from "../../redux/reducers/index";
 import {connect} from "react-redux";
 import PublicLoginContainer from "./containers/Login";
-import PublicRegisterContainer from "./containers/Register";
 import {PrivateRoute} from "../../components";
 import PublicProfileContainer from "./containers/Profile/index";
 
@@ -19,7 +18,6 @@ export default class PublicContainer extends React.Component<IProps> {
     return (
       <div>
         <Route path={`${match.url}/login`} component={PublicLoginContainer}/>
-        <Route path={`${match.url}/register`} component={PublicRegisterContainer}/>
         <PrivateRoute path={`${match.url}/profile`} component={PublicProfileContainer}/>
       </div>
     );
