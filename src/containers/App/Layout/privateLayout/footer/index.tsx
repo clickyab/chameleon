4e26a4bbebffd1b506247661bf4c0e6f9cdd9ad6
kasra <kasra.ansari@yahoo.com>
2017-08-27 14:12:02 +0430

    add footer component

---
+++ src/containers/App/Layout/privateLayout/footer/index.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import {Layout} from "antd";
+import I18n from "../../../../../services/i18n/index";
+
+const {Footer} = Layout;
+const i18n = I18n.getInstance();
+
+export const PrivateFooter = (): JSX.Element => (
+  <Footer>
+    <ul className="pFooter">
+      <li>{i18n._t("Clickyab@v1.1")}</li>
+      <li><a href="">{i18n._t("contact us")}</a></li>
+      <li><a href="">{i18n._t("faq")}</a></li>
+      <li><a href="">{i18n._t("rules")}</a></li>
+      <li><a href="">{i18n._t("blog")}</a></li>
+    </ul>
+  </Footer>
+);
+
