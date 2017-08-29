2f02dde0de8a5b354b95b7732514328c3e36c67f
alireza-mh <Alireza.mh@gmail.com>
2017-08-29 07:34:41 -0700

    Feature/profile (#16)
    * profile validation
    
    * profile validation
    
    * validation Profile added
    
    * Profile and badge fixed
    
    * fix
    
    * Config created (rtl Support)

--- src/containers/User/containers/Profile/index.tsx
+++ src/containers/User/containers/Profile/index.tsx
@@ -6,79 +6,134 @@ import {RootState} from "../../../../redux/reducers/index";
 import I18n from "../../../../services/i18n/index";
 import Translate from "../../../../components/i18n/Translate/index";
 import {UserApi, UserResponseLoginOKAccount} from "../../../../api/api";
-import {Form, Card, Row, message, Col, notification , Layout} from "antd";
+import {Form, Card, Row, message, Col, notification, Layout} from "antd";
 import {TextField, Checkbox, RaisedButton, FontIcon, Toggle, SelectField} from "material-ui";
 import {setUser, setIsLogin} from "../../../../redux/app/actions/index";
 import Icon from "../../../../components/Icon/index" ;
+import CONFIG from "../../../../constants/config" ;
+
+import "./style.less";
+
+export interface IProps extends RouteComponentProps<void> {
+    form: any;
+}
+
+export interface IState {
+    isDisable: boolean;
+    isCorporation: boolean;
+}
 
 const {Sider} = Layout;
 const FormItem = Form.Item;
 
 
-export namespace App {
-    export namespace Public {
-        export namespace Register {
-            export interface Props extends RouteComponentProps<void> {
-                form: any;
-            }
+export interface IProps extends RouteComponentProps<void> {
+    form: any;
+}
 
-            export interface State {
-                isDisable: boolean;
-            }
-        }
-    }
+export interface IState {
+    isDisable: boolean ;
+    isCorporation: boolean ;
 }
 
 
+
 @connect(mapStateToProps, mapDispatchToProps)
-export default class PublicProfileContainer extends React.Component<App.Public.Register.Props, App.Public.Register.State> {
+
+class PublicProfileContainer extends React.Component<IProps, IState> {
 
     private i18n = I18n.getInstance();
 
-    constructor(props: App.Public.Register.Props) {
+
+    constructor(props: IProps) {
         super(props);
 
         this.state = {
             isDisable: true,
+            isCorporation: false
         };
     }
     render() {
         const mailPlaceHolder = this.i18n._t("Email");
         const passwordPlaceHolder = this.i18n._t("Password");
-        // const {getFieldDecorator} = this.props.form;
+        const {getFieldDecorator} = this.props.form;
         return (
-            <div className="profile-container">
+            <div className={ ( CONFIG.DIR === "rtl" ) ? "profile-container-rtl" : "profile-container" }  >
                     <Row gutter={16} type="flex" align="top" justify="center">
                         <Col span={18}>
+                            {this.state.isCorporation &&
+                            <Row gutter={16} type="flex" align="top" >
+                                <Col span={8}>
+                                    <FormItem>
+                                        {getFieldDecorator("Corpration Name", {
+                                            rules: [{required: true, message: this.i18n._t("Please input your Submit Corpration Name!")}],
+                                        })(
+                                        <TextField
+                                            fullWidth={true}
+                                            floatingLabelText={this.i18n._t("Corpration Name")}
+                                        />)}
+                                    </FormItem>
+                                </Col>
+                                <Col span={8}>
+                                    <FormItem>
+                                        {getFieldDecorator("Submit number/ID", {
+                                            rules: [{required: true, message: this.i18n._t("Please input your Submit number/ID!")}],
+                                        })(
+                                        <TextField
+                                            fullWidth={true}
+                                            floatingLabelText={this.i18n._t("Submit number/ID")}
+                                        />)}
+                                    </FormItem>
+                                </Col>
+                                <Col span={8}>
+                                    <FormItem>
+                                        {getFieldDecorator("Economic code", {
+                                            rules: [{required: true, message: this.i18n._t("Please input your Submit Economic code!")}],
+                                        })(
+                                        <TextField
+                                            fullWidth={true}
+                                            floatingLabelText={this.i18n._t("Economic code")}
+                                        />)}
+                                    </FormItem>
+                                </Col>
+                            </Row>}
                             <Row gutter={16} type="flex" align="top" >
                             <Col span={12}>
                                 <FormItem>
+                                    {getFieldDecorator("Name", {
+                                        rules: [{required: true, message: this.i18n._t("Please input your Submit Name!")}],
+                                    })(
                                     <TextField
                                         fullWidth={true}
                                         floatingLabelText={this.i18n._t("Name")}
-                                        autoFocus={true}
-                                    />
+                                    />)}
                                 </FormItem>
                             </Col>
                             <Col span={12}>
                                 <FormItem>
+                                    {getFieldDecorator("Last name", {
+                                        rules: [{required: true, message: this.i18n._t("Please input your Submit last name!")}],
+                                    })(
                                     <TextField
                                         fullWidth={true}
-                                        floatingLabelText={this.i18n._t("Family")}
-                                    />
+                                        floatingLabelText={this.i18n._t("Last name")}
+                                    />)}
                                 </FormItem>
                             </Col>
                             </Row>
-                            <Row  gutter={16} className={ (this.state.isDisable) ? "column-disable" : ""}  type="flex" align="top" >
+                            <Row  gutter={16} className={ (this.state.isDisable) ? "column-disable" : "column-enable"}  type="flex" align="top" >
                                 <Col span={12}>
                                     <FormItem>
+                                        {getFieldDecorator("Email", {
+                                            rules: [{required: true, message: this.i18n._t("Please input your Submit Email!")}],
+                                        })(
                                         <TextField
                                             fullWidth={true}
                                             floatingLabelText={this.i18n._t("Email")}
                                             disabled={true}
-                                        />
+                                        />)}
                                     </FormItem>
-                                    <p className="enable-des">You can't change your Email. for changing your password <a onClick={() => {
+                                    <p className={ (this.state.isDisable) ? "enable-des" : "disable-des"}>You can't change your Email. for changing your password <a onClick={() => {
                                         this.setState({
                                             isDisable: !this.state.isDisable,
                                         });
@@ -86,32 +141,70 @@ export default class PublicProfileContainer extends React.Component<App.Public.R
                                 </Col>
                                 <Col span={12}>
                                     <FormItem>
+                                        {getFieldDecorator("password", {
+                                            rules: [{required: true, message: this.i18n._t("Please input your Submit password!")}],
+                                        })(
                                         <TextField
                                             fullWidth={true}
                                             floatingLabelText={this.i18n._t("Password")}
                                             type="password"
                                             disabled={this.state.isDisable}
-                                        />
+                                        />)}
                                     </FormItem>
                                 </Col>
                             </Row>
                             <Row gutter={16} type="flex" align="top" >
-                                <Col span={12}>
-                                    <FormItem>
+                            <Col span={12}>
+                                <FormItem>
+                                    {getFieldDecorator("Mobile", {
+                                        rules: [{required: true, message: this.i18n._t("Please input your Submit mobile!")}],
+                                    })(
                                         <TextField
                                             fullWidth={true}
                                             floatingLabelText={this.i18n._t("Mobile")}
-                                            autoFocus={true}
-                                        />
+                                        />)}
+                                </FormItem>
+                            </Col>
+                            <Col span={12}>
+                                <FormItem>
+                                    {getFieldDecorator("phone", {
+                                        rules: [{required: true, message: this.i18n._t("Please input your Submit phone!")}],
+                                    })(
+                                        <TextField
+                                            fullWidth={true}
+                                            floatingLabelText={this.i18n._t("phone")}
+                                        />)}
+                                </FormItem>
+                            </Col>
+                        </Row>
+                            <Row gutter={16} type="flex" align="top" >
+                                <Col span={12}>
+                                    <FormItem>
+                                            <TextField
+                                                fullWidth={true}
+                                                floatingLabelText={this.i18n._t("Post Address")}
+                                            />
                                     </FormItem>
                                 </Col>
                                 <Col span={12}>
                                     <FormItem>
-                                        <TextField
-                                            fullWidth={true}
-                                            floatingLabelText={this.i18n._t("Phone")}
-                                        />
+                                            <TextField
+                                                fullWidth={true}
+                                                floatingLabelText={this.i18n._t("Address")}
+                                                defaultValue="Defualt address goes here"
+                                                disabled={true}
+                                            />
                                     </FormItem>
+                                </Col>
+                            </Row>
+                            <Row gutter={16} type="flex"  align="top"  >
+                                <Col span={24} >
+                                <RaisedButton
+                                    type="Enter"
+                                    label={<Translate value="Save Changes"/>}
+                                    className="btn-save-change"
+                                    style={{color: "green"}}
+                                />
                                 </Col>
                             </Row>
                         </Col>
@@ -135,14 +228,15 @@ export default class PublicProfileContainer extends React.Component<App.Public.R
     }
 }
 function mapStateToProps(state: RootState) {
-  return {
-    /* empty */
-  };
+    return {
+        /* empty */
+    };
 }
 
 function mapDispatchToProps(dispatch) {
-  return {
-    /* empty */
-  };
+    return {
+        /* empty */
+    };
 }
 
+export default Form.create()(PublicProfileContainer);
