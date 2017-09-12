67e1df191f2ad4b263acfb4b4cc32843659f3429
Alireza <alireza.mh@gmail.com>
2017-09-12 06:31:07 -0700

    Fix inline style -  UI of UserBox added

--- src/components/PasswordStrength/index.tsx
+++ src/components/PasswordStrength/index.tsx
@@ -70,7 +70,7 @@ export default class PasswordStrength extends React.Component<IProps, IState> {
 
   public render() {
     return (
-      <div style={{position: "relative"}}>
+      <div className="password-strength">
         <Icon className="strength-meter" type={this.state.strengthIcon}/>
         <TextField {...this.props} onKeyUp={this.checkStrength}/>
       </div>
