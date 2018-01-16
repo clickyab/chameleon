11fe068f7180e0b0cbc38690888fb2f8e6be0e01
alireza-mh <Alireza.mh@gmail.com>
2018-01-16 13:19:10 +0200

    Add RangePicker Wrapper component (#117)
    * Add RangePicker Wrapper component
    
    * outside click will hide datepicker

---
+++ src/components/RangePickerWrapper/index.tsx
@@ -0,0 +1,86 @@
+import * as React from "react";
+import "./style.less";
+import RangePicker , {rangeType} from "../RangePicker";
+import * as moment from "moment-jalaali";
+import onClickOutside from "react-onclickoutside";
+import I18n from "../../services/i18n/index";
+
+export interface IRangeObject {
+    range: {
+        from: moment.type;
+        to: moment.type;
+    };
+    type: rangeType;
+}
+
+interface IProps {
+    onChange?: (value: IRangeObject) => void;
+    value?: IRangeObject | string;
+}
+
+interface IState {
+    value: IRangeObject;
+    selectedDay: any;
+    currentMonth: moment.type;
+    enterSecond: boolean;
+    isGregorian: boolean;
+    display: boolean;
+}
+
+
+
+class RangePickerWrapper extends React.Component<IProps, IState> {
+    private i18n = I18n.getInstance();
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value ? props.value : moment(),
+            selectedDay: [],
+            currentMonth: moment(props.value),
+            isGregorian: props.isGregorian ? props.isGregorian : false,
+            enterSecond: false,
+            display: false,
+        };
+    }
+    private handleClick() {
+        this.setState({display: !this.state.display});
+    }
+
+    private handleChange(value) {
+        if (value.type === "custom") {
+            let temp = value;
+            temp.type = moment(temp.range.from).format("jYYYY/jM/jD") + this.i18n._t(" to ").toString() + ((temp.range.to) ? moment(temp.range.to).format("jYYYY/jM/jD") : "");
+            if (this.props.onChange) {
+                this.props.onChange(value);
+            }
+            this.setState({value: temp});
+        }
+        else {
+            this.setState({value});
+            if (this.props.onChange) {
+                this.props.onChange(value);
+            }
+        }
+    }
+    private handleClickOutside(evt) {
+        this.setState({display: false});
+    }
+
+    render() {
+        return (
+            <div className="range-wrapper" >
+                <div className="range-wrapper-input" onClick={this.handleClick.bind(this)}>
+                    <input value={this.i18n._t(this.state.value.type).toString()}/>
+                </div>
+                {this.state.display &&
+                <div className="range-inside-wrapper">
+                    <RangePicker {...this.props} onChange={(value) => this.handleChange(value)}/>
+                </div>
+                }
+            </div>
+        );
+    }
+}
+
+export default onClickOutside(RangePickerWrapper);
--- src/containers/App/Dashboard/index.tsx
+++ src/containers/App/Dashboard/index.tsx
@@ -1,29 +1,55 @@
 import * as React from "react";
 import I18n from "../../../services/i18n/index";
 import {connect} from "react-redux";
+import RangePicker from "../../../components/RangePicker";
+import * as moment from "moment";
 import {setBreadcrumb} from "../../../redux/app/actions/index";
+import RangePickerWrapper from "../../../components/RangePickerWrapper";
 
 interface IProps {
     setBreadcrumb: (name: string, title: string, parent: string) => void;
 }
-
+interface IState {
+  value: any;
+}
+export enum rangeType { TODAY, CUSTOM }
+const Range = {
+    range: {
+        from: moment(),
+          to: moment().add("day", 1),
+         },
+    type: rangeType.CUSTOM
+};
 @connect(null, mapDispatchToProps)
-export default class Dashboard extends React.Component<IProps> {
-    private i18n = I18n.getInstance();
+export default class Dashboard extends React.Component<IProps , IState> {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value : moment().toISOString()
+    };
+  }
+  private i18n = I18n.getInstance();
 
-    public componentDidMount() {
-        this.props.setBreadcrumb("dashboard", this.i18n._t("Dashboard").toString(), "home");
-    }
+  public componentDidMount() {
+    this.props.setBreadcrumb("dashboard", this.i18n._t("Dashboard").toString(), "home");
+  }
 
-    public render() {
-        return (
-            <div>Dashboard</div>
-        );
-    }
+  public render() {
+    let getRange: any ;
+    return (
+        <div>
+            <RangePickerWrapper
+                onChange={value => {this.setState({value}) ; console.log(value); }}
+                value={Range}
+            />
+      <div>Dashboard</div>
+        </div>
+    );
+  }
 }
 
 function mapDispatchToProps(dispatch) {
-    return {
-        setBreadcrumb: (name: string, title: string, parent: string) => dispatch(setBreadcrumb({name, title, parent})),
-    };
+  return {
+    setBreadcrumb: (name: string, title: string, parent: string) => dispatch(setBreadcrumb({name, title, parent})),
+  };
 }
