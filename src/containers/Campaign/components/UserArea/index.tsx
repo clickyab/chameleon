c39efe9ef2bdb35594db2743f3dc0e295cac2eee
Ehsan <zerob4wl@gmail.com>
2018-05-03 16:57:48 +0430

    app transaction history

--- src/containers/Campaign/components/UserArea/index.tsx
+++ src/containers/Campaign/components/UserArea/index.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import {Redirect, RouteComponentProps, Switch, withRouter} from "react-router";
-import {Link} from "react-router-dom";
 import {PrivateRoute} from "../../../../components/PrivateRoute/index";
 import PublicProfileContainer from "../../../User/containers/Profile/index";
 import ChargeContainer from "../../../User/containers/Charge/index";
+import TransactionHistory from "../../../User/containers/TransactionHistory/index";
 import Avatar from "../../../../components/Avatar/index";
 import {connect} from "react-redux";
 import {RootState} from "../../../../redux/reducers/index";
-import {UserApi, UserAvatarPayload, UserResponseLoginOKAccount, UserUserPayload} from "../../../../api/api";
-import {notification , Tabs , Row} from "antd/lib";
+import {UserApi, UserAvatarPayload, UserResponseLoginOKAccount} from "../../../../api/api";
+import {notification, Tabs, Row} from "antd/lib";
 import I18n from "../../../../services/i18n/index";
 import {setUser} from "../../../../redux/app/actions/index";
 import CONFIG from "../../../../constants/config";
@@ -20,13 +20,14 @@ import FlowUpload from "../../../../services/Upload/flowUpload";
 import {UPLOAD_MODULES} from "../../../Campaign/components/UploadFile";
 
 const TabPane = Tabs.TabPane;
+
 /**
  * @interface
  * @desc define component props
  */
 interface IOwnProps {
-  match?: any;
-  history?: any;
+    match?: any;
+    history?: any;
 }
 
 enum COMP_TAB_USR {
@@ -37,59 +38,60 @@ enum COMP_TAB_USR {
 }
 
 interface IProps extends RouteComponentProps<void> {
-  user: UserResponseLoginOKAccount;
-  setUser: (user: UserResponseLoginOKAccount) => {};
-  activeTab: string;
+    user: UserResponseLoginOKAccount;
+    setUser: (user: UserResponseLoginOKAccount) => {};
+    activeTab: string;
 }
 
 interface IState {
-  user: UserResponseLoginOKAccount;
-  uploadProgress: number;
-  activeTab: string;
+    user: UserResponseLoginOKAccount;
+    uploadProgress: number;
+    activeTab: string;
 }
 
 @connect(mapStateToProps, mapDispatchToProps)
 class UserArea extends React.Component<IProps, IState> {
 
-  private i18n = I18n.getInstance();
+    private i18n = I18n.getInstance();
 
-  constructor(props: IProps) {
-    super(props);
-    this.state = {
-      user: props.user,
-      uploadProgress: null,
-      activeTab: props.activeTab ? props.activeTab : "Profile",
-    };
-  }
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            user: props.user,
+            uploadProgress: null,
+            activeTab: props.activeTab ? props.activeTab : "Profile",
+        };
+    }
 
-  componentWillReceiveProps(props: IProps) {
-    this.setState({
-      user: props.user,
-    });
-    this.handleTabUrl(this.props.history.location.pathname);
-  }
+    componentWillReceiveProps(props: IProps) {
+        this.setState({
+            user: props.user,
+        });
+        this.handleTabUrl(this.props.history.location.pathname);
+    }
+
     private handleTabUrl(url: string): void {
-      let splitUrl = (url.split("/"));
-      let tempKey: string;
-      switch (splitUrl[splitUrl.length - 1]) {
-          case "profile":
-              tempKey = "profile";
-          break;
-          case "transaction":
-              tempKey = "transaction";
-              break;
-          case "charge":
-              tempKey = "charge";
-              break;
-          case "logout":
-              tempKey = "logout";
-              break;
-      }
-      console.log(tempKey);
+        let splitUrl = (url.split("/"));
+        let tempKey: string;
+        switch (splitUrl[splitUrl.length - 1]) {
+            case "profile":
+                tempKey = "profile";
+                break;
+            case "transactions":
+                tempKey = "transactions";
+                break;
+            case "charge":
+                tempKey = "charge";
+                break;
+            case "logout":
+                tempKey = "logout";
+                break;
+        }
         this.setState({
             activeTab: tempKey,
         });
     }
+
     private handleTab(key): void {
         this.setState({
             activeTab: key,
@@ -97,83 +99,89 @@ class UserArea extends React.Component<IProps, IState> {
         this.props.history.push("/user/" + key.toString());
     }
 
-  uploadAvatar(file) {
-    const uploader = new FlowUpload(UPLOAD_MODULES.USER_AVATAR, file);
-    uploader.upload((state) => {
-      // todo:: show progress
-      this.setState({
-        uploadProgress: state.progress,
-      });
-    })
-      .then((res) => {
-        const userApi = new UserApi();
-        let userAvatarPayload: UserAvatarPayload = {
-          avatar: res.url,
-        };
+    uploadAvatar(file) {
+        const uploader = new FlowUpload(UPLOAD_MODULES.USER_AVATAR, file);
+        uploader.upload((state) => {
+            // todo:: show progress
+            this.setState({
+                uploadProgress: state.progress,
+            });
+        })
+            .then((res) => {
+                const userApi = new UserApi();
+                let userAvatarPayload: UserAvatarPayload = {
+                    avatar: res.url,
+                };
 
-        userApi.userAvatarPut({
-          payloadData: userAvatarPayload,
-        }).then((data) => {
-          notification.success({
-            message: this.i18n._t("Upload Avatar"),
-            className: (CONFIG.DIR === "rtl") ? "notif-rtl" : "",
-            description: this.i18n._t("Your avatar changed successfully.").toString(),
-          });
-          this.props.setUser(data.account);
-          this.setState({
-            user: data.account,
-            uploadProgress: null,
-          });
-        });
+                userApi.userAvatarPut({
+                    payloadData: userAvatarPayload,
+                }).then((data) => {
+                    notification.success({
+                        message: this.i18n._t("Upload Avatar"),
+                        className: (CONFIG.DIR === "rtl") ? "notif-rtl" : "",
+                        description: this.i18n._t("Your avatar changed successfully.").toString(),
+                    });
+                    this.props.setUser(data.account);
+                    this.setState({
+                        user: data.account,
+                        uploadProgress: null,
+                    });
+                });
 
-      })
-      .catch((error) => {
-        notification.error({
-          message: this.i18n._t("Upload Avatar").toString(),
-          className: (CONFIG.DIR === "rtl") ? "notif-rtl" : "",
-          description: this.i18n._t("Error in change avatar.").toString(),
-        });
-        this.setState({
-          uploadProgress: null,
-        });
-      });
-  }
-  render() {
-    const {match} = this.props;
-    return (
-      <div dir={CONFIG.DIR} className="user-area">
-        <div className="avatar-wrapper" >
-          {this.state.user && <div className="avatar-click" onClick={ () => {document.getElementById("uploadAvatar").click(); console.log("clicked"); } } >
-          <Avatar user={this.state.user} className="user-area-avatar avatar-hover" radius={32} progress={0}  />
-          </div>}
-          <input style={{display: "none"}} id="uploadAvatar" type="file"
-                 onChange={(e) => this.uploadAvatar(e.target.files[0])} ref="avatar"
-                 accept="image/*"/>
-          <h2>{this.state.user.first_name} {this.state.user.last_name}</h2>
-        </div>
-        <Switch>
-            <Row type="flex" align="middle">
-            <Tabs activeKey={this.state.activeTab}
-                  onChange={this.handleTab.bind(this)}
-                  type="editable-card"
-                  hideAdd={true}
-                  className="tabs-container mt-2 mr-4 ml-4">
-                <TabPane tab={this.i18n._t("Profile")} key="profile" closable={false}>
-                    <PrivateRoute path={`${match.url}/profile`} component={PublicProfileContainer}/>
-                </TabPane>
-                <TabPane tab={this.i18n._t("Transaction History")} key="transaction" closable={false}>
-                </TabPane>
-                <TabPane tab={this.i18n._t("Charge Account")} key="charge" closable={false}>
-                    <PrivateRoute path={`${match.url}/charge`} component={ChargeContainer}/>
-                </TabPane>
-            </Tabs>
-          {/*<Redirect to="/dashboard"/>*/}
-            </Row>
-        </Switch>
-      </div>
-    )
-      ;
-  }
+            })
+            .catch((error) => {
+                notification.error({
+                    message: this.i18n._t("Upload Avatar").toString(),
+                    className: (CONFIG.DIR === "rtl") ? "notif-rtl" : "",
+                    description: this.i18n._t("Error in change avatar.").toString(),
+                });
+                this.setState({
+                    uploadProgress: null,
+                });
+            });
+    }
+
+    render() {
+        const {match} = this.props;
+        return (
+            <div dir={CONFIG.DIR} className="user-area">
+                <div className="avatar-wrapper">
+                    {this.state.user && <div className="avatar-click" onClick={() => {
+                        document.getElementById("uploadAvatar").click();
+                        console.log("clicked");
+                    }}>
+                        <Avatar user={this.state.user} className="user-area-avatar avatar-hover" radius={32}
+                                progress={0}/>
+                    </div>}
+                    <input style={{display: "none"}} id="uploadAvatar" type="file"
+                           onChange={(e) => this.uploadAvatar(e.target.files[0])} ref="avatar"
+                           accept="image/*"/>
+                    <h2>{this.state.user.first_name} {this.state.user.last_name}</h2>
+                </div>
+                <Switch>
+                    <Row type="flex" align="middle">
+                        <Tabs activeKey={this.state.activeTab}
+                              onChange={this.handleTab.bind(this)}
+                              type="editable-card"
+                              hideAdd={true}
+                              className="tabs-container mt-2 mr-4 ml-4">
+                            <TabPane tab={this.i18n._t("Profile")} key="profile" closable={false}>
+                                <PrivateRoute path={`${match.url}/profile`} component={PublicProfileContainer}/>
+                            </TabPane>
+                            <TabPane tab={this.i18n._t("Transaction History")} key="transactions" closable={false}>
+                                <PrivateRoute path={`${match.url}/transactions`} component={TransactionHistory}/>
+                            </TabPane>
+                            <TabPane tab={this.i18n._t("Charge Account")} key="charge" closable={false}>
+                                <PrivateRoute path={`${match.url}/charge`} component={ChargeContainer}/>
+                            </TabPane>
+                        </Tabs>
+                        {/*<Redirect to="/dashboard"/>*/}
+                    </Row>
+                </Switch>
+            </div>
+        )
+            ;
+    }
 }
 
 
@@ -185,11 +193,11 @@ class UserArea extends React.Component<IProps, IState> {
  * @returns {{currentStep: STEPS; selectedCampaignId: number; match: any; history: any}}
  */
 function mapStateToProps(state: RootState, ownProps: IOwnProps) {
-  return {
-    user: state.app.user,
-    match: ownProps.match,
-    history: ownProps.history,
-  };
+    return {
+        user: state.app.user,
+        match: ownProps.match,
+        history: ownProps.history,
+    };
 }
 
 
@@ -201,9 +209,9 @@ function mapStateToProps(state: RootState, ownProps: IOwnProps) {
  * @returns {{currentStep: STEPS; selectedCampaignId: number; match: any; history: any}}
  */
 function mapDispatchToProps(dispatch) {
-  return {
-    setUser: (user: UserResponseLoginOKAccount) => dispatch(setUser(user)),
-  };
+    return {
+        setUser: (user: UserResponseLoginOKAccount) => dispatch(setUser(user)),
+    };
 }
 
 // export component and use withRouter to access route properties
---
+++ src/containers/User/containers/TransactionHistory/index.tsx
@@ -0,0 +1,115 @@
+/**
+ * @file Charge file
+ */
+import * as React from "react";
+import {connect} from "react-redux";
+import {
+    ControllersApi,
+    UserResponseLoginOKAccount
+} from "../../../../api/api";
+import {RootState} from "../../../../redux/reducers/index";
+import I18n from "../../../../services/i18n/index";
+import {setUser, setBreadcrumb, unsetBreadcrumb} from "../../../../redux/app/actions/index";
+import Icon from "../../../../components/Icon/index" ;
+import CONFIG from "../../../../constants/config" ;
+import "./style.less";
+import {ISelectBoxItem} from "../../../Campaign/containers/Naming/Components/SelectBox";
+import DataTableChartWrapper from "../../../../components/DataTableChartWrapper";
+
+/**
+ * @interface IProps
+ * @desc define Prop object
+ */
+export interface IProps {
+    form: any;
+    user: UserResponseLoginOKAccount;
+    setBreadcrumb: (name: string, title: string, parent: string) => void;
+    unsetBreadcrumb: (name: string) => void;
+}
+
+/**
+ * @interface IState
+ * @desc define state object
+ */
+export interface IState {
+    selectedPayment: PAYMENT;
+    amountValue: number | null;
+    accountDeposit: number | null;
+    couponInput: number | "";
+    JSXForm: JSX.Element;
+}
+
+enum PAYMENT { ONLINE = "online", RECEIPT = "receipt", CHECK_BANK = "check bank", COUPON = "coupon"}
+
+@connect(mapStateToProps, mapDispatchToProps)
+
+class TransactionHistory extends React.Component<IProps, IState> {
+
+    private i18n = I18n.getInstance();
+    disable: boolean = false;
+    controllerApi = new ControllersApi();
+
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            selectedPayment: PAYMENT.ONLINE,
+            amountValue: null,
+            accountDeposit: props.user.balance ? props.user.balance : null,
+            couponInput: "",
+            JSXForm: null,
+        };
+    }
+
+    PaymentTypes: ISelectBoxItem[] = [
+        {
+            title: this.i18n._t("Online payment").toString(),
+            value: PAYMENT.ONLINE,
+            icon: <Icon name="cif-money-charge" className={"campaign-icon"}/>,
+        },
+        {
+            title: this.i18n._t("With bank receipt").toString(),
+            value: PAYMENT.RECEIPT,
+            icon: <Icon name="cif-bankwire" className={"campaign-icon"}/>,
+        },
+        // {
+        //     title: this.i18n._t("Coupon").toString(),
+        //     value: PAYMENT.COUPON,
+        //     icon: <Icon name="cif-browser-campaign-outline" className={"campaign-icon"}/>,
+        // }
+    ];
+
+    componentDidMount() {
+        this.props.setBreadcrumb("charge", this.i18n._t("Transaction History").toString(), "home");
+    }
+
+    render() {
+        return (
+            <div className={(CONFIG.DIR === "rtl") ? "charge-container-rtl" : "charge-container"}>
+                <DataTableChartWrapper
+                    name="campaignDetails"
+                    chartDataFn={this.controllerApi.financialGraphSpendGet}
+                    chartDefinitionFn={this.controllerApi.financialBillingDefinitionGet}
+                    dataTableDefinitionFn={this.controllerApi.financialBillingDefinitionGet}
+                    dataTableDataFn={this.controllerApi.financialBillingGet.bind(this)}
+                    showRangePicker={true}
+                />
+            </div>
+        );
+    }
+}
+
+function mapStateToProps(state: RootState) {
+    return {
+        user: state.app.user,
+    };
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        setUser: (user: UserResponseLoginOKAccount) => dispatch(setUser(user)),
+        setBreadcrumb: (name: string, title: string, parent: string) => dispatch(setBreadcrumb({name, title, parent})),
+        unsetBreadcrumb: (name: string) => dispatch(unsetBreadcrumb(name)),
+    };
+}
+
+export default TransactionHistory;
